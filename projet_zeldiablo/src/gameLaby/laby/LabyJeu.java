27e3acdf8ac1da3aef7c2e1d77f79b93af01bc0a
Matthieu Petelot <matthieu.petelot5@etu.univ-lorraine.fr>
2025-05-16 09:52:04 +0200

    [Create] creation du labyjeu

---
+++ projet_zeldiablo/src/gameLaby/laby/LabyJeu.java
@@ -0,0 +1,46 @@
+package gameLaby.laby;
+
+import moteurJeu.Clavier;
+import moteurJeu.Jeu;
+
+import java.io.IOException;
+
+public class LabyJeu implements Jeu{
+
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 600;
+
+    public final Labyrinthe labyrinthe;
+
+    public LabyJeu() throws IOException {
+        this.labyrinthe = new Labyrinthe("laby1.txt");
+    }
+
+    public void update(double seconde, Clavier clavier) {
+        if (clavier.gauche) {
+            labyrinthe.deplacerPerso(Labyrinthe.GAUCHE);
+        }
+
+        if (clavier.droite) {
+            labyrinthe.deplacerPerso(Labyrinthe.DROITE);
+        }
+
+        if (clavier.haut) {
+            labyrinthe.deplacerPerso(Labyrinthe.HAUT);
+        }
+
+        if (clavier.bas) {
+            labyrinthe.deplacerPerso(Labyrinthe.BAS);
+        }
+
+    }
+
+    public void init() {
+        //rien de ce que je pense
+    }
+
+    public boolean etreFini() {
+        return labyrinthe.etreFini();
+    }
+
+}
