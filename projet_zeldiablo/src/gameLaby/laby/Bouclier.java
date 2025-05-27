c368e71faa68ba77d9c4c55f16f400c7e931e53e
Matthieu Cabot <matthieu.cabot3@etu.univ-lorraine.fr>
2025-05-27 11:56:13 +0200

    [CREATE] Création de la classe Bouclier

--- projet_zeldiablo/src/gameLaby/laby/Bouclier.java
+++ projet_zeldiablo/src/gameLaby/laby/Bouclier.java
@@ -1,4 +1,45 @@
 package gameLaby.laby;
 
-public class Bouclier {
+/**
+ * Classe qui représente un objet bouclier
+ */
+public class Bouclier extends Objet{
+
+    /**
+     * Nom du bouclier
+     */
+    String nom;
+
+    /**
+     * Point de résistance du bouclier
+     */
+    int def;
+
+    /**
+     * constructeur de bouclier
+     * @param nom nom du bouclier
+     * @param def résistance du bouclier
+     * @param x position X
+     * @param y position Y
+     */
+    public Bouclier(String nom, int def, int x, int y) {
+        this.nom = nom;
+        this.def = def;
+        this.X = x;
+        this.Y = y;
+    }
+
+    /**
+     * @return le nom du bouclier
+     */
+    public String getNom(){
+        return this.nom;
+    }
+
+    /**
+     * @return la résistance du bouclier
+     */
+    public int getDef(){
+        return this.def;
+    }
 }
