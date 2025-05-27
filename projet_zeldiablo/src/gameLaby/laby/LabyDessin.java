29570bf5f5f2b73dd0981e37bdbfb5f7f96a3cce
Matthieu Petelot <matthieu.petelot5@etu.univ-lorraine.fr>
2025-05-27 11:56:39 +0200

    [Modify] ajout de l'affichage inventaire

--- projet_zeldiablo/src/gameLaby/laby/LabyDessin.java
+++ projet_zeldiablo/src/gameLaby/laby/LabyDessin.java
@@ -40,18 +40,30 @@ public class LabyDessin implements DessinJeu {
         gc.fillOval(perso.x*echelle, perso.y*echelle, echelle, echelle);
 
         //gestion de l'inventaire
-        gc.setFill(Color.DARKGREY);
+        gc.setFill(Color.BLUE);
         gc.fillRect(0,labyJeu.getLabyrinthe().getLengthY()*LabyDessin.size,labyJeu.getLabyrinthe().getLength()*LabyDessin.size, LabyJeu.INTERFACE_HEIGHT);
 
         //position de la ligne de démarcation infosJoueur/inventaire
-        double x1 = (double) (labyJeu.getLabyrinthe().getLength() /3)*LabyDessin.size;
-        double y1 = labyJeu.getLabyrinthe().getLengthY()*LabyDessin.size;
-        double y2 = (labyJeu.getLabyrinthe().getLengthY() + LabyJeu.INTERFACE_HEIGHT)*LabyDessin.size;
+        double x1 = (double) (labyJeu.getLabyrinthe().getLength() /3)*size;
+        double y1 = labyJeu.getLabyrinthe().getLengthY()*size;
+        double y2 = (labyJeu.getLabyrinthe().getLengthY() + LabyJeu.INTERFACE_HEIGHT)*size;
         gc.strokeLine(x1,y1,x1,y2);
 
         //gestion inventaire
+        int border = 5;
+        int marge = 2;
+        int caseSize = 60;
+        gc.setFill(Color.RED);
+        gc.fillRect(x1+border, y1+border,6*marge+5*caseSize,2*border+caseSize);
 
-
+        gc.setFill(Color.BLACK);
+        //creation des carrés
+        int x = (int) ((x1+marge+border));
+        int y = (int) ((y1+marge+border));
+        for (int i = 0; i < 5; i++) {
+            gc.fillRect(x, y, caseSize,caseSize);
+            x = x + caseSize+marge;
+        }
     }
 
 }
