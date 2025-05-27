72275eafad4aad4cd0197c8bd283392e9d741510
GPOIRE <gpoire.developpement@gmail.com>
2025-05-27 11:51:39 +0200

    [Create] Création de la classe Sortie qui définie la case pour finir le niveau

---
+++ projet_zeldiablo/src/gameLaby/laby/Sortie.java
@@ -0,0 +1,16 @@
+package gameLaby.laby;
+
+/**
+ * Classe Sortie implements CaseDeclencheuse et définit la case qui permet de finir le niveau
+ * @author GPOIRE-Developpement
+ */
+public class Sortie implements CaseDeclencheuse{
+    /**
+     * Méthode activer définit les actions à faire lorsqu'un joueur entre sur la case de fin
+     * @param ent
+     */
+    public void activer(Entite ent){
+        if(!(ent instanceof Perso)) return;
+        System.out.println("Vous avez terminé");
+    }
+}
