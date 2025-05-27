b1e92620193a65c204f038c6b3a30326eb165049
GPOIRE <gpoire.developpement@gmail.com>
2025-05-27 11:03:27 +0200

    [Create] Création de la classe CasePiege correspondant à la représentation d'une case piégé

---
+++ projet_zeldiablo/src/gameLaby/laby/CasePiege.java
@@ -0,0 +1,7 @@
+package gameLaby.laby;
+
+public class CasePiege implements CaseDeclencheuse{
+    public void activer(Entite ent){
+        System.out.println("Vous êtes sur une case piégée !");
+    }
+}
