3ae987a7499ef46870469b0f782d186583af77ef
Matthieu Petelot <matthieu.petelot5@etu.univ-lorraine.fr>
2025-05-27 22:34:56 +0200

    [Modify] ajout d'un effet de profondeur avec les murs

--- projet_zeldiablo/src/gameLaby/laby/LabyDessin.java
+++ projet_zeldiablo/src/gameLaby/laby/LabyDessin.java
@@ -14,14 +14,14 @@ import java.util.ArrayList;
 
 public class LabyDessin implements DessinJeu {
 
-    static final int size = 50;
+    static final double size = 50;
 
     static final String PATH = "texture/";
-    static final String WALL = PATH + "wall/simple_wall.png";
+    static final String WALL = PATH + "wall/";
     static final String TOWER = PATH + "wall/tower.png";
     static final String GROUND = PATH + "ground/simple_grass.png";
 
-    static final String PERSO = PATH + "character/";
+    static final String PERSO = PATH + "character/plague/";
     static final String MONSTER = PATH + "monster/";
 
     static final String ITEM = PATH + "item/";
@@ -29,7 +29,7 @@ public class LabyDessin implements DessinJeu {
 
     public void dessinerJeu(Jeu jeu, Canvas canvas) {
 
-        int echelle = size;
+        double echelle = size;
 
         LabyJeu labyJeu = (LabyJeu) jeu;
 
@@ -38,7 +38,7 @@ public class LabyDessin implements DessinJeu {
         gc.setFill(Color.BLACK);
         Labyrinthe laby = labyJeu.getLabyrinthe();
 
-        File imgf_wall = new File(WALL);
+        File imgf_wall = new File(WALL + "wall.png");
         Image img_wall = new Image(imgf_wall.toURI().toString());
 
         File imgf_ground = new File(GROUND);
@@ -46,47 +46,47 @@ public class LabyDessin implements DessinJeu {
 
         for (int x = 0; x < laby.getLength(); x++) {
             for (int y = 0; y < laby.getLengthY(); y++) {
-                if (laby.getMur(x,y)) {
-                    gc.drawImage(img_wall, x*size, y*size, size, size);
+                if (laby.getMur(x, y)) {
+                    gc.drawImage(img_wall, x * size, y * size, size, size);
                 } else {
-                    gc.drawImage(img_ground, x*size, y*size, size, size);
+                    gc.drawImage(img_ground, x * size, y * size, size, size);
                 }
             }
         }
 
         //gestion de l'inventaire
         gc.setFill(Color.BLUE);
-        gc.fillRect(0,labyJeu.getLabyrinthe().getLengthY()*LabyDessin.size,labyJeu.getLabyrinthe().getLength()*LabyDessin.size, LabyJeu.INTERFACE_HEIGHT);
+        gc.fillRect(0, labyJeu.getLabyrinthe().getLengthY() * LabyDessin.size, labyJeu.getLabyrinthe().getLength() * LabyDessin.size, LabyJeu.INTERFACE_HEIGHT);
 
         //position de la ligne de démarcation infosJoueur/inventaire
-        double x1 = (double) (labyJeu.getLabyrinthe().getLength() /3)*size;
-        double y1 = labyJeu.getLabyrinthe().getLengthY()*size;
-        double y2 = (labyJeu.getLabyrinthe().getLengthY() + LabyJeu.INTERFACE_HEIGHT)*size;
-        gc.strokeLine(x1,y1,x1,y2);
+        double x1 = (double) (labyJeu.getLabyrinthe().getLength() / 3) * size;
+        double y1 = labyJeu.getLabyrinthe().getLengthY() * size;
+        double y2 = (labyJeu.getLabyrinthe().getLengthY() + LabyJeu.INTERFACE_HEIGHT) * size;
+        gc.strokeLine(x1, y1, x1, y2);
 
         //gestion inventaire
         int border = 5;
         int marge = 2;
         int caseSize = 60;
         gc.setFill(Color.RED);
-        gc.fillRect(x1+border, y1+border,6*marge+5*caseSize,border+caseSize);
+        gc.fillRect(x1 + border, y1 + border, 6 * marge + 5 * caseSize, border + caseSize);
 
         gc.setFill(Color.BLACK);
         //creation des carrés
-        int x = (int) ((x1+marge+border));
-        int y = (int) ((y1+marge+border));
+        int x = (int) ((x1 + marge + border));
+        int y = (int) ((y1 + marge + border));
         for (int i = 0; i < 5; i++) {
-            gc.fillRect(x, y, caseSize,caseSize);
-            x = x + caseSize+marge;
+            gc.fillRect(x, y, caseSize, caseSize);
+            x = x + caseSize + marge;
         }
 
         //on parcours les cases
         CaseDeclencheuse[][] cases = labyJeu.getLabyrinthe().getCase();
-        for (int colonne=0; colonne<cases.length; colonne++) {
-            for (int ligne=0; ligne<cases[colonne].length; ligne++) {
+        for (int colonne = 0; colonne < cases.length; colonne++) {
+            for (int ligne = 0; ligne < cases[colonne].length; ligne++) {
                 if (cases[colonne][ligne] != null) {
                     gc.setFill(Color.ORANGE);
-                    gc.fillRect(colonne*size+3, ligne*size+3, size-6, size-6);
+                    gc.fillRect(colonne * size + 3, ligne * size + 3, size - 6, size - 6);
                 }
             }
         }
@@ -98,18 +98,16 @@ public class LabyDessin implements DessinJeu {
             //creation du fichier à partir du path
             File imgf_item = new File(path);
             Image img_item = new Image(imgf_item.toURI().toString());
-            gc.drawImage(img_item, objet.getX()*size, objet.getY()*size, size, size);
+            gc.drawImage(img_item, objet.getX() * size, objet.getY() * size, size, size);
         }
 
-        //dessin du monstre
-
-
+        //dessin des monstre
         ArrayList<Entite> monstres = labyJeu.getLabyrinthe().getMonstres();
         for (Entite entite : monstres) {
             String path_monster = MONSTER + entite.getPosition() + ".png";
             File imgf_monster = new File(path_monster);
             Image img_monster = new Image(imgf_monster.toURI().toString());
-            gc.drawImage(img_monster, entite.getX()*size, entite.getY()*size, size, size);
+            gc.drawImage(img_monster, entite.getX() * size, entite.getY() * size, size, size);
         }
 
         //gestion joueur
@@ -120,8 +118,25 @@ public class LabyDessin implements DessinJeu {
         Image img_pj = new Image(imgf_pj.toURI().toString());
         int posX = pj.getX();
         int posY = pj.getY();
-        gc.drawImage(img_pj, posX*size, posY*size, size, size);
+        gc.drawImage(img_pj, posX * size, posY * size, size, size);
 
+        //mur de base si pas etendu
+        File imgf_side_wall = new File(WALL+"wall_side.png");
+        Image img_side_wall = new Image(imgf_side_wall.toURI().toString());
+
+        File imgf_side_wall_extend = new File(WALL+"wall_side_extend.png");
+        Image img_side_wall_extend = new Image(imgf_side_wall_extend.toURI().toString());
+        //gestion de la "3D" avec la face haute des murs
+        for (int n = 0; n < laby.getLength(); n++) {
+            for (int p = 0; p < laby.getLengthY(); p++) {
+                if (laby.getMur(n, p)) {
+                    if (p > 0 && !laby.getMur(n, p - 1)) {
+                        gc.drawImage(img_side_wall,n * size , (p * size) - size / 3, size, size / 3);
+                    } else {
+                        gc.drawImage(img_side_wall_extend,n * size, (p * size) - size, size, size);
+                    }
+                }
+            }
+        }
     }
-
 }
