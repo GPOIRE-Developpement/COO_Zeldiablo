f71b45628281d9813401fa90e6f421abeec06e80
Matthieu Cabot <matthieu.cabot3@etu.univ-lorraine.fr>
2025-05-27 11:55:44 +0200

    [CREATE] Création de la classe Epee

--- projet_zeldiablo/src/gameLaby/laby/Epee.java
+++ projet_zeldiablo/src/gameLaby/laby/Epee.java
@@ -1,4 +1,46 @@
 package gameLaby.laby;
 
-public class Epee {
+
+/**
+ * Classe qui représente un objet Epée
+ */
+public class Epee extends Objet{
+
+    /**
+     * Nom de l'épée
+     */
+    String nom;
+
+    /**
+     * Degat de l'épée
+     */
+    int dommage;
+
+    /**
+     * constructeur de Epee
+     * @param nom nom de l'épée
+     * @param dommage degat de l'épée
+     * @param x position en X
+     * @param y position en Y
+     */
+    public Epee(String nom, int dommage, int x, int y) {
+        this.nom = nom;
+        this.dommage = dommage;
+        this.X = x;
+        this.Y = y;
+    }
+
+    /**
+     * @return le nom de l'épée
+     */
+    public String getNom(){
+        return this.nom;
+    }
+
+    /**
+     * @return les dégats de l'épée
+     */
+    public int getDommage(){
+        return this.dommage;
+    }
 }
