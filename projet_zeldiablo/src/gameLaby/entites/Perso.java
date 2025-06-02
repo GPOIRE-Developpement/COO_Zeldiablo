6b71960a64873dd6367bab60b99a19be44d27013
Matthieu Cabot <matthieu.cabot3@etu.univ-lorraine.fr>
2025-06-02 19:14:37 +0200

    [Add] Ajout de la javadoc de Perso et Potion

--- projet_zeldiablo/src/gameLaby/entites/Perso.java
+++ projet_zeldiablo/src/gameLaby/entites/Perso.java
@@ -10,8 +10,16 @@ import java.util.List;
  */
 public class Perso extends Entite {
 
+    /**
+     * Liste d'objet qui représente l'inventaire du joueur
+     */
     private ArrayList<Objet> inventaire = new ArrayList<>();
+
+    /**
+     * Représente l'objet qui est séléctionné dans l'inventaire
+     */
     private Objet itemSelecte;
+
     /**
      * constructeur
      *
@@ -22,6 +30,10 @@ public class Perso extends Entite {
         super(dx, dy);
     }
 
+    /**
+     * Méthode permettant d'interagir avec les objets présent dans le labyrinthe ou dans l'inventaire
+     * @param objet Liste des objets d'un labyrinthe
+     */
     public void interagir(List<Objet> objet) {
         Objet objetAttraper = null;
         for (Objet obj : objet){
@@ -44,24 +56,42 @@ public class Perso extends Entite {
         }
     }
 
+    /**
+     * Permet de séléctionner un objet présent dans l'inventaire
+     * @param indice indice de l'objet à selectionner
+     */
     public void selectionnerObjet(int indice) {
         if (indice >= 1 && indice <= inventaire.size()) {
             itemSelecte = inventaire.get(indice-1);
         }
     }
 
+    /**
+     * @return retourne l'objet qui est séléctionné dans l'inventaire
+     */
     public Objet getItemSelecte() {
         return itemSelecte;
     }
 
+    /**
+     * @return retourne l'inventaire du personnage
+     */
     public ArrayList<Objet> getInventaire() {
         return inventaire;
     }
 
+    /**
+     * Permet au personnage d'attaquer
+     * @param entite l'entite à attaquer
+     */
     public void attaquer(Entite entite) {
         entite.setHP(-atk);
     }
 
+    /**
+     * Modifie le nombre d'Hp du perso
+     * @param hp Nouveaux Hp
+     */
     @Override
     public void setHP(int hp){
         super.setHP(hp);
--- projet_zeldiablo/src/gameLaby/objets/Potion.java
+++ projet_zeldiablo/src/gameLaby/objets/Potion.java
@@ -1,20 +1,43 @@
 package gameLaby.objets;
 
+/**
+ * Classe qui représente un objet Potion
+ */
 public class Potion extends Objet {
 
+    /**
+     * Vie rendu par la potion
+     */
     private int vie;
+
+    /**
+     * Indique si la potion est pleine ou vide
+     */
     private boolean used;
 
+    /**
+     * Constructeur de Potion
+     * @param nom Nom de la potion
+     * @param vie Vie rendu par la potion
+     * @param posX Position X de la potion
+     * @param posY position Y de la potion
+     */
     public Potion(String nom, int vie, int posX, int posY) {
         super(nom, posX, posY);
         this.vie = vie;
         used = false;
     }
 
+    /**
+     * @return la vie rendu par la potion
+     */
     public int getVie() {
         return vie;
     }
 
+    /**
+     * @return le nom de la potion
+     */
     public String getNom() {
         if (used) {
             return "empty_bottle";
@@ -23,6 +46,9 @@ public class Potion extends Objet {
         }
     }
 
+    /**
+     * @return indique si la potion est utilisé ou non
+     */
     public boolean isUsed() {
         return used;
     }
