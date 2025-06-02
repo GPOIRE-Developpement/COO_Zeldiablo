6448561dffa4430ddb2b32e6e7d4d0dc5788274f
Gauthier Dartoy--Moinaux <e88969u@etu.univ-lorraine.fr>
2025-06-02 14:42:02 +0200

    [add] création de la classe du nouveau monstre

--- projet_zeldiablo/src/gameLaby/entites/Fantome.java
+++ projet_zeldiablo/src/gameLaby/entites/Fantome.java
@@ -1,5 +1,21 @@
 package gameLaby.entites;
 
-public class Fantome {
+public class Fantome extends Entite {
+	/**
+	 * constructeur
+	 *
+	 * @param posx position selon x
+	 * @param posy position selon y
+	 */
+	public Fantome(int posx, int posy) {
+		super(posx, posy);
+		this.atk = 1;
+		this.hp = 5;
+	}
+
+	@Override
+	public void attaquer(Entite entite) {
+		entite.setHP(-1);
+	}
 
 }
