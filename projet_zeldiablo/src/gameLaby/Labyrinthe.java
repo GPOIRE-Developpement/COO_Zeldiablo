8184df4632a39223011c4e3811dd6fef2dc99529
Gauthier Dartoy--Moinaux <e88969u@etu.univ-lorraine.fr>
2025-06-02 14:49:21 +0200

    [modify] création de la méthode generationFantome

--- projet_zeldiablo/src/gameLaby/Labyrinthe.java
+++ projet_zeldiablo/src/gameLaby/Labyrinthe.java
@@ -2,6 +2,7 @@ package gameLaby;
 
 import gameLaby.casesSpe.*;
 import gameLaby.entites.Entite;
+import gameLaby.entites.Fantome;
 import gameLaby.entites.Monstre;
 import gameLaby.entites.Perso;
 import gameLaby.objets.Bouclier;
@@ -61,6 +62,10 @@ public class Labyrinthe {
      */
     private int nbMonstre = 4;
 
+    /*
+     * nombre de fantome voulus
+     */
+    private int nbFantome = 2;
 
      private ArrayList<Porte> portes;
 
@@ -363,5 +368,22 @@ public class Labyrinthe {
             }
         }
         return true;
+    }
+    /**
+     * Methode permettant de gérer la génération aléatoire des fantômes
+     * @param nbColonne nombre de colonnes du laby
+     * @param nbLigne nombre de lignes du laby
+     */
+    public void generationFantome(int nbColonne, int nbLigne) {
+        for (int i = 0; i < nbFantome; i++) {
+            int x = (int) Math.floor(Math.random() * nbColonne);
+            int y = (int) Math.floor(Math.random() * nbLigne);
+            while (this.murs[x][y] || pj.getX() == x && pj.getY() == y) {
+                x = (int) Math.floor(Math.random() * nbColonne);
+                y = (int) Math.floor(Math.random() * nbLigne);
+            }
+            Fantome fantome = new Fantome(x, y);
+            monstres.add(fantome);
+        }
     }
 }
