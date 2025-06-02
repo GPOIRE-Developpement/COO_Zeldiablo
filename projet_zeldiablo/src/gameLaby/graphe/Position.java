d776349ba9b5d2cffc1e84a3865905a3905baedc
GPOIRE-Developpement <gpoire.developpement@gmail.com>
2025-06-02 23:25:15 +0200

    [Add] Ajout de la classe Position

--- projet_zeldiablo/src/gameLaby/graphe/Position.java
+++ projet_zeldiablo/src/gameLaby/graphe/Position.java
@@ -1,4 +1,38 @@
 package gameLaby.graphe;
 
 public class Position {
+    private int x;
+    private int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(o instanceof Position){
+            Position p = (Position)o;
+            if(p.getX() == x && p.getY() == y){
+                return true;
+            }
+        }
+        return false;
+    }
 }
