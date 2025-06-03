df95677d8fa97bdf9aa46ad698c3bf02ae063aaf
GPOIRE <gpoire.developpement@gmail.com>
2025-06-03 09:08:25 +0200

    [Add] Ajout de la méthode calculerChemin

--- projet_zeldiablo/src/gameLaby/graphe/Valeurs.java
+++ projet_zeldiablo/src/gameLaby/graphe/Valeurs.java
@@ -2,95 +2,49 @@ package gameLaby.graphe;
 
 import java.util.*;
 
-/**
- * Classe fournie, permet de stocker des valeurs associées au noeud et
- * des parents
- * - un noeud est represente par un String (son nom)
- * - on accede avec des get (getValeur et getParent)
- * - on modifie avec des set (setValeur et setParent)
- */
 public class Valeurs {
-    /**
-     * attributs pour stocker les informations
-     * (type Table = Dictionnaire)
-     * dans le programme de 2 annee.
-     */
-    Map<String, Double> valeur;
-    Map<String, String> parent;
-    /**
-     * constructeur vide (initialise la possibilité de stocker
-     * des valeurs)
-     */
+
+    Map<Position, Double> valeur;
+    Map<Position, Position> parent;
+
     public Valeurs() {
         this.valeur = new TreeMap<>();
         this.parent = new TreeMap<>();
     }
-    /**
-     * permet d'associer une valeur a un nom de noeud (ici L(X))
-     *
-     * @param nom le nom du noeud
-     * @param valeur la valeur associée
-     */
-    public void setValeur(String nom, double valeur) {
-// modifie valeur
-        this.valeur.put(nom, valeur);
-    }
-/**
- * permet d'associer un parent a un nom de noeud (ici parent(X))
- *
- * @param nom nom du noeud
- * @param parent nom du noeud parent associe
- */
 
-    public void setParent(String nom, String parent) {
-        this.parent.put(nom, parent);
+    public void setValeur(Position pos, double valeur) {
+        this.valeur.put(pos, valeur);
     }
-    /**
-     * accede au parent stocke associe au noeud nom passe en
-     * parametre
-     *
-     * @param nom nom du noeud
-     * @return le nom du noeud parent
-     */
-    public String getParent(String nom) {
-        return this.parent.get(nom);
+
+    public void setParent(Position pos, Position posParent) {
+        this.parent.put(pos, posParent);
     }
-    /**
-     * accede a la valeur associee au noeud nom passe en
-     * parametre
-     *
-     * @param nom nom du noeud
-     * @return la valeur stockee
-     */
-    public double getValeur(String nom) {
-        return this.valeur.get(nom);
+
+    public Position getParent(Position pos) {
+        return this.parent.get(pos);
     }
-    /**
-     * retourne une chaine qui affiche le contenu
-     * - par noeud stocke
-     * - a chaque noeud, affiche la valeur puis le noeud
-     * parent
-     *
-     * @return descriptif du noeud
-     */
+
+    public double getValeur(Position pos) {
+        return this.valeur.get(pos);
+    }
+
+    @Override
     public String toString() {
         String res = "";
-// pour chaque noeud
-        for (String s : this.valeur.keySet()) {
-// ajoute la valeur et le noeud parent
+        for (Position s : this.valeur.keySet()) {
             Double valeurNoeud = valeur.get(s);
-            String noeudParent = parent.get(s);
+            Position noeudParent = parent.get(s);
             res += s + " -> V:" + valeurNoeud + " p:" + noeudParent + "\n";
 
         }
         return res;
     }
 
-    public List<String> calculerChemin(String destination){
+    public List<Position> calculerChemin(Position posDest){
         boolean depart = false;
-        List<String> chemin = new ArrayList<>();
-        chemin.add(destination);
-        String last = this.getParent(destination);
+        List<Position> chemin = new ArrayList<>();
+        chemin.add(posDest);
+        Position last = this.getParent(posDest);
         while(!depart){
             if(last.equals(this.getParent(last))){
                 chemin.add(last);
