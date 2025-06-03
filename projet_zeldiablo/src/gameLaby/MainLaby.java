e352ff600455669d2eed89357bcc8a141a692f96
GPOIRE <gpoire.developpement@gmail.com>
2025-06-03 09:54:50 +0200

    [Modify] Modification du mainLaby pour test orque

--- projet_zeldiablo/src/gameLaby/MainLaby.java
+++ projet_zeldiablo/src/gameLaby/MainLaby.java
@@ -17,7 +17,7 @@ public class MainLaby {
         niveaux.add("labySimple/laby2.txt");
         niveaux.add("labySimple/laby3.txt");
 
-        LabyJeu labyJeu = new LabyJeu(niveaux);
+        LabyJeu labyJeu = new LabyJeu("labySimple/laby2.txt");
         LabyDessin labyDessin = new LabyDessin();
 
         MoteurJeu.setTaille(labyJeu.getLabyrinthe().getLength()*LabyDessin.size, labyJeu.getLabyrinthe().getLengthY()*LabyDessin.size + LabyJeu.INTERFACE_HEIGHT);
